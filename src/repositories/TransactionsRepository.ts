480edfa2f333b29f8131976c0ebab92ebf996de5
lucasoc <lucas.oconti@gmail.com>
2020-08-12 16:20:54 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((value, transaction) => {
+      if (transaction.type === 'income') return (value += transaction.value);
+      return value;
+    }, 0);
+
+    const outcome = this.transactions.reduce((value, transaction) => {
+      if (transaction.type === 'outcome') return (value += transaction.value);
+      return value;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
